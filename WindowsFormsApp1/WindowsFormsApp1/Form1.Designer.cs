30ac9f32cb2c894e63cd9080e1bc1cfd07b2b919
jakonthebridge <30919373+jakonthebridge@users.noreply.github.com>
2020-12-25 16:06:37 +0900

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.inquireButton = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.itemCodeTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(28, 335);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(100, 50);
+            this.axKHOpenAPI1.TabIndex = 0;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(28, 24);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "로그인";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(138, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(269, 304);
+            this.listBox1.TabIndex = 2;
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Location = new System.Drawing.Point(28, 63);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(75, 24);
+            this.logoutButton.TabIndex = 3;
+            this.logoutButton.Text = "로그아웃";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            // 
+            // inquireButton
+            // 
+            this.inquireButton.Location = new System.Drawing.Point(713, 12);
+            this.inquireButton.Name = "inquireButton";
+            this.inquireButton.Size = new System.Drawing.Size(75, 23);
+            this.inquireButton.TabIndex = 4;
+            this.inquireButton.Text = "조회";
+            this.inquireButton.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(474, 87);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(314, 220);
+            this.listBox2.TabIndex = 5;
+            // 
+            // itemCodeTextBox
+            // 
+            this.itemCodeTextBox.Location = new System.Drawing.Point(554, 14);
+            this.itemCodeTextBox.Name = "itemCodeTextBox";
+            this.itemCodeTextBox.Size = new System.Drawing.Size(100, 21);
+            this.itemCodeTextBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.itemCodeTextBox);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.inquireButton);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.axKHOpenAPI1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Button inquireButton;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox itemCodeTextBox;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.loginButton.Click += this.buttonClicked; //상태 변화1
+            this.logoutButton.Click += this.buttonClicked;
+            this.inquireButton.Click += this.buttonClicked;
+            this.axKHOpenAPI1.OnEventConnect += this.axKHOpenAPI_OnEventConnect;
+            this.axKHOpenAPI1.OnReceiveTrData += this.axKHOpenAPI_OnReceiveTrData;
+        }
+
+        public void buttonClicked(object sender, EventArgs e) //로그인버튼. 클릭 이벤트시 버튼이 눌려졌음으로 변화
+        {
+            if (sender.Equals(this.loginButton))
+            {
+                if (axKHOpenAPI1.CommConnect() == 0)
+                    this.listBox1.Items.Add("로그인 시작");
+                else
+                    this.listBox1.Items.Add("로그인 실패");
+            }
+            else if(sender.Equals(this.logoutButton))
+            {
+                axKHOpenAPI1.CommTerminate();
+                this.listBox1.Items.Add("로그아웃");
+            }
+            else if(sender.Equals(this.inquireButton))
+            {
+                axKHOpenAPI1.SetInputValue("종목코드", this.itemCodeTextBox.Text.Trim());
+
+                int nRet = axKHOpenAPI1.CommRqData("주식기본정보", "OPT10001", 0, "1001");
+                if (nRet == 0)
+                    this.listBox1.Items.Add("주식 정보요청 성공");
+                else
+                    this.listBox1.Items.Add("주식 정보요청 실패");
+            }
+        }
+        private void axKHOpenAPI_OnEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            if (e.nErrCode == 0)
+            {
+                this.listBox1.Items.Add("로그인 성공");
+                if (this.axKHOpenAPI1.GetConnectState() == 1)
+                    this.listBox1.Items.Add("접속상태:연결중");
+                else if (this.axKHOpenAPI1.GetConnectState() == 0)
+                    this.listBox1.Items.Add("접속상태:미연결");
+            }
+            else
+                this.listBox1.Items.Add("로그인 실패");
+        }
+
+        private void axKHOpenAPI_OnReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            if(e.sRQName == "주식기본정보")
+            {
+                int nCnt = axKHOpenAPI1.GetRepeatCnt(e.sTrCode, e.sRQName);
+                for (int nIdx = 0; nIdx < nCnt; nIdx++)
+                {
+                    this.listBox2.Items.Add("종목코드" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "종목코드"));
+                    this.listBox2.Items.Add("종목명" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "종목명"));
+                    this.listBox2.Items.Add("거래량" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "거래량"));
+                    this.listBox2.Items.Add("시가" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "시가"));
+                    this.listBox2.Items.Add("고가" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "고가"));
+                    this.listBox2.Items.Add("저가" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "저가"));
+                    this.listBox2.Items.Add("현재가" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "현재가"));
+                    this.listBox2.Items.Add("등략율" + axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "등략율"));
+                }
+            }
+        }
+
+        
+    }
+
+
+}
